4aeff922b230c2d9d5feea27cf9e69d17b80e6fe
Poomipat <poomipatboonkong@gmail.com>
2025-12-09 17:21:00 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,29 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i = 0;
+    int number[100];
+    while(true){
+        cout << "Enter an integer: ";
+        cin >> number[i];
+        if(number[i] != 0){
+            i++;
+        }else{
+            break;
+        }
+    }
+
+    int E = 0;
+    int O = 0;
+    for(int j = 0; j < i; j++){
+        if(number[j]%2 == 0){
+            E++;
+        }else{
+            O++;
+        }
+    }
+        cout << "#Even numbers = " << E << "\n";
+        cout << "#Odd numbers = " << O;
+
     return 0;
 }
